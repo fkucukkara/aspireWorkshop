333884cc6ea476ac5484cd396a235634749506ff
Fatih Kucukkara <f.kucukkara@gmail.com>
2025-01-05 14:04:57 +0100

    Add implementation and update gitignore

--- src/aspireWorkshop.API/Program.cs
+++ src/aspireWorkshop.API/Program.cs
@@ -4,15 +4,16 @@ using aspireWorkshop.API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddProblemDetails();
 builder.AddSqlServerDbContext<PostContext>("sqlDb");
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-
 builder.AddServiceDefaults();
-
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+app.UseExceptionHandler();
+app.UseStatusCodePages();
 app.MapDefaultEndpoints();
 
 if (app.Environment.IsDevelopment())
@@ -25,10 +26,48 @@ app.UseHttpsRedirection();
 
 var v1 = app.MapGroup("/aspireWorkshop/v1/posts");
 
-v1.MapGet("/", async (IUnitOfWork unitOfWork) =>
+v1.MapGet("/", async Task<IResult> (IUnitOfWork unitOfWork) =>
 {
-    var products = await unitOfWork.Repository<Post>().GetAllAsync();
-    return TypedResults.Ok(products);
+    return TypedResults.Ok(await unitOfWork.Repository<Post>().GetAllAsync());
+});
+
+v1.MapGet("/{id}", async Task<IResult> (IUnitOfWork unitOfWork, int id) =>
+{
+    return await unitOfWork.Repository<Post>().GetByIdAsync(id)
+        is Post post
+            ? TypedResults.Ok(post)
+            : TypedResults.NotFound();
+});
+
+v1.MapPost("/", async Task<IResult> (IUnitOfWork unitOfWork, Post post) =>
+{
+    await unitOfWork.Repository<Post>().AddAsync(post);
+    await unitOfWork.SaveChangesAsync();
+
+    return TypedResults.Created($"/aspireWorkshop/v1/posts/{post.Id}", post);
+});
+
+v1.MapPut("/{id}", async Task<IResult> (IUnitOfWork unitOfWork, int id, Post post) =>
+{
+    var existingPost = await unitOfWork.Repository<Post>().GetByIdAsync(id);
+    if (existingPost is null) return TypedResults.NotFound();
+
+    existingPost.Content = post.Content;
+    await unitOfWork.SaveChangesAsync();
+
+    return TypedResults.NoContent();
+});
+
+v1.MapDelete("/{id}", async Task<IResult> (IUnitOfWork unitOfWork, int id) =>
+{
+    if (await unitOfWork.Repository<Post>().GetByIdAsync(id) is Post post)
+    {
+        unitOfWork.Repository<Post>().Remove(post);
+        await unitOfWork.SaveChangesAsync();
+        return TypedResults.NoContent();
+    }
+
+    return TypedResults.NotFound();
 });
 
 app.Run();
